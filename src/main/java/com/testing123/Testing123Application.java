5727a738b3b8dcc7b7a0cc02ed0abc52aa0b3ee7
pavansaipoluru7@gmail.com <pavan2001.ps@gmail.com>
2025-11-29 18:53:30 +0530

    first commit

---
+++ src/main/java/com/testing123/Testing123Application.java
@@ -0,0 +1,13 @@
+package com.testing123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testing123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testing123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/testing123/Testing123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testing123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
